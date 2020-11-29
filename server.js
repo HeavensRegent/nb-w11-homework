857e4ee33fe75c1f0d6acb186573c2dae2659902
darthNater <blstarwars@live.com>
2020-11-29 11:36:32 -0700

    Notes are getting pulled, saving, and deleting

--- server.js
+++ server.js
@@ -7,30 +7,60 @@ const PORT = 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "public/notes.html"))
+});
+
+app.get("/api/notes", function(req, res) {
+    return res.json(getApiNotes());
 });
 
 app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
+app.post("/api/notes", function(req, res) {
+    saveApiNotes(req.body);
+    return res.json({message: "Note saved successfully"});
+});
+
+app.delete("/api/notes/:id", function(req, res) {
+    deleteApiNote(req.params.id);
+    return res.json({message: "Node deleted successfully"});
+})
+
 function getApiNotes() {
-    let notes = json.PARSE(readFromFile('db.json'));
+    let notes = JSON.parse(readFromFile('db.json'));
     console.log(notes);
+    return notes;
 }
 
 function saveApiNotes(note) {
     let notes = getApiNotes();
 
+    let indexOfNote = notes.findIndex(e => note.id && e.id === note.id);
     //Replace .id with an appropriate identifier
-    notes.id = note;
-    writeToFile('db.json', notes);
+    if(indexOfNote === -1)
+        notes.push({...note, id: generateId(note.title)});
+    else
+        notes[indexOfNote] = note;
+    
+    console.log(notes);
+    writeToFile('db.json', JSON.stringify(notes));
 }
 
 function deleteApiNote(id) {
+    let notes = getApiNotes();
 
+    let indexOfNote = notes.findIndex(e => id && e.id === id);
+    if(indexOfNote !== -1)
+    {
+        notes.splice(indexOfNote, 1);
+
+        writeToFile('db.json', JSON.stringify(notes));
+    }
 }
 
 // function to write README file
@@ -53,6 +83,13 @@ function readFromFile(fileName) {
     } catch(e) {
         console.log(e);
     }
+    return;
+}
+
+function generateId(title) {
+    title = title.split(" ").join("");
+    title += Date.now();
+    return title;
 }
 
 app.listen(PORT, function() {
